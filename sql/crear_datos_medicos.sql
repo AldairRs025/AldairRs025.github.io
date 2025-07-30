3dc0b6d741879bb48843ebed58d97224d9cd6634
Aldair R's <70624080+AldairRs025@users.noreply.github.com>
2025-07-29 21:17:55 -0600

    Delete sql directory

--- sql/crear_datos_medicos.sql
+++
@@ -1,8 +0,0 @@
-
-CREATE TABLE datos_medicos (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    prescripcion VARCHAR(10),
-    deficiencia TEXT,
-    observaciones_higiene TEXT,
-    fecha_registro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
